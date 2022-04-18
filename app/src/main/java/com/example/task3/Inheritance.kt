e3914e4ce20959f5d0b368c52244be492cecbf0a
Omar Juma <jumao2669@gmail.com>
2022-04-18 11:17:54 +0300

    Class is disturbing

---
+++ app/src/main/java/com/example/task3/Inheritance.kt
@@ -0,0 +1,5 @@
+package com.example.task3
+
+class Inheritance {
+
+}
--- app/src/main/java/com/example/task3/MainActivity.kt
+++ app/src/main/java/com/example/task3/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.task3
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +17,9 @@ class MainActivity : AppCompatActivity() {
         val image3=findViewById<ImageView>(R.id.imageView3)
        fun Roll() {
            val sides=6
-           val randomNo1=(1..sides).random()
-           val randomNo2=(1..sides).random()
-           val randomNo3=(1..sides).random()
+           var randomNo1=(1..sides).random()
+           var randomNo2=(1..sides).random()
+           var randomNo3=(1..sides).random()
 
            when(randomNo1){
                1 -> println(image1.setImageResource(R.drawable.one))
@@ -44,9 +45,31 @@ class MainActivity : AppCompatActivity() {
                5 -> println(image3.setImageResource(R.drawable.five))
                6 -> println(image3.setImageResource(R.drawable.six))
            }
+           if(randomNo1==6 && randomNo2==6 || randomNo1==6 && randomNo3==6 || randomNo2==6 && randomNo3==6) {
+               Toast.makeText(applicationContext, "You win", Toast.LENGTH_SHORT).show()
+           }
+           else if(randomNo1+randomNo2==9 || randomNo1+randomNo3==9 || randomNo2+randomNo3==9){
+               Toast.makeText(applicationContext, "You lose", Toast.LENGTH_SHORT).show()
+           }
+
        }
         btn.setOnClickListener(){
         Roll()
         }
+        val car1=Car("Toyota","Pink")
+        val car2=Car("Mercedes Benz","Grey")
+        val txt: TextView =findViewById(R.id.textView1)
+        val txt2:TextView=findViewById(R.id.textView2)
+        val priceOfCar1=car1.Price()
+        val priceOfCar2=car2.Price()
+        txt.text=priceOfCar1.toString()
+        txt2.text= priceOfCar2.toString()
     }
+}
+class Car( val name:String ,val color:String){
+    fun Price() {
+        println("Your Car name is ${name} and its ${color} in color")
+        //println((900000..3000000).random())
+    }
+
 }
